e2fa6849af9f47b030724c009112ac337747435f
kangwongu <kwg0527@naver.com>
2021-08-13 11:36:24 +0900

    dp1

---
+++ code/dynamic_programming.py
@@ -0,0 +1,9 @@
+def f_dp(n):
+    cache = [ 0 for index in range(n + 1) ]
+    cache[0] = 0
+    cache[1] = 1
+    
+    for index in range(2, n + 1):
+        cache[index] = cache[index - 1] + cache[index - 2]
+        
+    return cache[index]
